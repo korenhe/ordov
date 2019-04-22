32af93521c522eb2423ec5feb39e17075dc9ea4d
Koren He <korenhe@outlook.com>
2019-04-23 01:56:34 +0800

    Add candidate detail page
    Use cards display the basic information.
    Add an empty api.html for collecting info from 3rdparty (Form post).

--- recruit_manager/urls.py
+++ recruit_manager/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.index, name='t_index'), # t_index will be used in template
     path('candidates/', views.CandidateTable.as_view(), name='t_candidates'),
     path('interviews/', views.InterviewTable.as_view(), name='t_interviews'),
+    path('interview-api/<int:candidate_id>', views.interview_api, name='t_interview_api'),
+    path('candidate/<int:pk>', views.CandidateDetail.as_view(), name='t_candidate'),
 ]
--- recruit_manager/views.py
+++ recruit_manager/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
 from candidates.models import Candidate
@@ -15,6 +15,10 @@ class CandidateTable(generic.ListView):
     def get_queryset(self):
         return Candidate.objects.all().prefetch_related('interview_set')
 
+class CandidateDetail(generic.DetailView):
+    model = Candidate
+    context_object_name = 't_candidate'
+    template_name = 'recruit_manager/detail_candidate.html'
 
 class InterviewTable(generic.ListView):
     context_object_name = 't_interview_list'
@@ -22,3 +26,10 @@ class InterviewTable(generic.ListView):
 
     def get_queryset(self):
         return Interview.objects.all()[:5]
+
+def interview_api(request, candidate_id):
+    candidate = get_object_or_404(Candidate, pk=candidate_id)
+    context = {
+        't_candidate': candidate
+    }
+    return render(request, 'recruit_manager/interview_api.html', context)
