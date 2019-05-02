4461997b5371cb2380b61f178468f23ceab37a5e
Treestein Hon <honpey@gmail.com>
2019-05-02 14:54:35 +0800

    resume: add more fields

--- resumes/models.py
+++ resumes/models.py
@@ -3,31 +3,45 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
-from .choices import (EDUCATION_CHOICES, BIRTH_YEAR_CHOICES, MAJOR_CHOICES)
+from .choices import (EDUCATION_CHOICES, BIRTH_YEAR_CHOICES, MAJOR_CHOICES, MARRIAGE_CHOICES)
 from candidates.models import Candidate
 
 # Create your models here.
 
 class Resume(models.Model):
     # The Resume would be linked to a candidate afterwards
-    # This resume would
     candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE, blank=True)
     resume_id = models.IntegerField()
     visible = models.BooleanField(default=False)
+
     # base info
     username = models.CharField(max_length=8)
-    gender = models.CharField(choices=(("m", "male"), ("f", "female"),), max_length=1, blank=True, null=True)
+    gender = models.CharField(choices=(("m", "男"), ("f", "女"),), max_length=1, blank=True, null=True)
     birth_year = models.CharField(max_length=4, blank=True, null=True, choices=BIRTH_YEAR_CHOICES)
     birth_month= models.CharField(max_length=4, blank=True, null=True)
     date_of_birth = models.DateTimeField(blank=True, null=True)
     identity = models.CharField(max_length=25, blank=True, null=True)
+    age = models.IntegerField()
+
+    # birth place (jiguan in chinese)
+    birth_provice = models.CharField(max_length=10, blank=True, null=True)
+    birth_city = models.CharField(max_length=10, blank=True, null=True)
+    birth_distinct = models.CharField(max_length=10, blank=True, null=True)
+    birth_place = models.CharField(max_length=50, blank=True, null=True)
 
     # social info
     phone_number = PhoneNumberField(null=True, blank=True, unique=True)
     qq = models.BigIntegerField(null=True, blank=True)
     residence = models.CharField(max_length=50, blank=True, null=True)
     email = models.CharField(max_length=50, blank=True, null=True)
-    marriaged = models.BooleanField(default=False)
+    marriage = models.CharField(max_length=10, blank=True, null=True, choices=MARRIAGE_CHOICES)
+
+    # Expection info
+    expected_area = models.CharField(max_length=50, null=True, blank=True)
+    expected_salary = models.CharField(max_length=50, null=True, blank=True)
+    expected_post = models.CharField(max_length=50, null=True, blank=True)
+    expected_positon = models.CharField(max_length=50, null=True, blank=True)
+
 
     # education related
     degree = models.CharField(max_length=30, blank=True, null=True)
