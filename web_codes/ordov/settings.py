b49ad22720ee3207449a74e03ad70a92d1442e26
Koren He <korenhe@outlook.com>
2019-05-02 20:01:25 +0800

    Use mysql instead sqlite for commercial use
    Use mysql-connector-python as the database backend and add docker_launcher
    folder for docker compose up.

--- web_codes/ordov/settings.py
+++ web_codes/ordov/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'ordov.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'mysql_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db', # later for DB host IP
+        'PORT': '3306',
     }
 }
 
