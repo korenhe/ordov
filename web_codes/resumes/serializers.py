5a3d9f2590cdde67a382cc4b4af6be950687b890
Treestein Hon <honpey@gmail.com>
2019-05-16 08:51:21 +0800

    update resume/education serilizer for more stable foreign key

--- web_codes/resumes/serializers.py
+++ web_codes/resumes/serializers.py
@@ -65,16 +65,31 @@ class ResumeSerializer(serializers.ModelSerializer):
 #            resume = Resume.objects.create(**validated_data)
 #            return resume
 
-        candidate_data = validated_data.pop('candidate')
-        resume = Resume.objects.create(**validated_data)
-        print(resume)
-        return resume
+        # create resume, currently we use the phone as key to identify one resume
+        resumeTarget = None
+        try:
+            phone=validated_data['phone_number']
+            if phone == '':
+                return None
+            resumeTarget = Resume.objects.get(phone_number=phone)
+        except:
+            print("resume item exit")
+            pass
+        if resumeTarget is None:
+            candidate_data = validated_data.pop('candidate')
+            resume = Resume.objects.create(**validated_data)
+            return resume
+        else:
+            return resumeTarget
 
 class EducationSerializer(serializers.ModelSerializer):
+
+    resume = ResumeSerializer(required=True)
+
     class Meta:
         model = Education
         fields = (
-            'resume',
+            'resume', # foreginkey
             'start',
             'end',
             'school',
@@ -90,3 +105,11 @@ class EducationSerializer(serializers.ModelSerializer):
             'instructor',
             'instructor_phone',
         )
+    def create(self, validated_data):
+        resume_data = validated_data.pop('resume')
+        resume = ResumeSerializer.create(ResumeSerializer(), validated_data=resume_data)
+
+        education, created = Education.objects.update_or_create(
+            resume=resume,**validated_data
+        )
+        return education
