e5187f531e376427dac65c25ed6a6eda45f5ab66
Treestein Hon <honpey@gmail.com>
2019-07-20 17:14:23 +0800

    Import django_extensions to show all the url info

--- web_codes/ordov/settings.py
+++ web_codes/ordov/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.weibo',
+    'django_extensions',
 
     'rest_framework',
     'rest_framework_datatables',
