50f87b66e34d9a6ad8ae883d610ff4c6b0d223ea
Treestein Hon <honpey@gmail.com>
2019-09-15 14:53:43 +0800

    Add weibo third_party login

--- web_codes/accounts/views.py
+++ web_codes/accounts/views.py
@@ -10,8 +10,52 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from accounts.models import UserProfile
 from allauth.account.views import LoginView
+from rest_framework import status as rest_status
+from rest_framework.response import Response
+
+import requests
+
 
 # Create your views here.
+
+def get_user_info(result):
+    """
+    获取用户信息
+    """
+    get_user_url = "https://api.weibo.com/2/users/show.json"
+    uid = result["uid"]
+    access_token = result["access_token"]
+    user_info = requests.get(url=get_user_url, params={"access_token": access_token, "uid": uid})
+    return user_info
+
+def process(request):
+    code = request.GET.get("code")
+    print("--->code", code)
+    result = get_token(code)  # 获取access_token和用户uid
+    result = result.json()
+    print("result: ", result)
+    userinfo = get_user_info(result).json()
+    print("user_info", userinfo)
+
+	# A Typical UserInfo Returned From Weibo
+    # user_info {'id': 7032578234, 'idstr': '7032578234', 'class': 1, 'screen_name': 'bigjson90',
+	# 'name': 'bigjson90', 'province': '11', 'city': '1000', 'location': '������', 'description': '',
+	# 'url': '', 'profile_image_url': 'https://tvax1.sinaimg.cn/default/images/default_avatar_female_50.gif?KID=imgbed,tva&Expires=1568458610&ssig=B2Bh8y5PL2',
+	# 'profile_url': 'u/7032578234', 'domain': '', 'weihao': '', 'gender': 'f', 'followers_count': 2,
+	# 'friends_count': 60, 'pagefriends_count': 0, 'statuses_count': 1, 'video_status_count': 0,
+	# 'favourites_count': 0, 'created_at': 'Thu Mar 14 22:46:39 +0800 2019', 'following': False,
+	# 'allow_all_act_msg': False, 'geo_enabled': True, 'verified': False, 'verified_type': -1,
+	# 'remark': '', 'insecurity': {'sexual_content': False},
+	# 'status': {'created_at': 'Thu Mar 14 22:48:11 +0800 2019', 'id': 4349876726770469, 'idstr': '4349876726770469', 'mid': '4349876726770469', 'can_edit': False, 'show_additional_indication': 0, 'text': 'hello', 'textLength': 5, 'source_allowclick': 0, 'source_type': 1, 'source': '<a href="http://app.weibo.com/t/feed/6vtZb0" rel="nofollow">������ weibo.com</a>', 'favorited': False, 'truncated': False, 'in_reply_to_status_id': '', 'in_reply_to_user_id': '', 'in_reply_to_screen_name': '', 'pic_urls': [], 'geo': None, 'is_paid': False, 'mblog_vip_type': 0, 'reposts_count': 0, 'comments_count': 0, 'attitudes_count': 0, 'pending_approval_count': 0, 'isLongText': False, 'reward_exhibition_type': 0, 'hide_flag': 0, 'mlevel': 0, 'visible': {'type': 0, 'list_id': 0}, 'biz_feature': 0, 'hasActionTypeCard': 0, 'darwin_tags': [], 'hot_weibo_tags': [], 'text_tag_tips': [], 'mblogtype': 0, 'rid': '0', 'userType': 0, 'more_info_type': 0, 'positive_recom_flag': 0, 'content_auth': 0, 'gif_ids': '', 'is_show_bulletin': 2, 'comment_manage_info': {'comment_permission_type': -1, 'approval_comment_type': 0},'pic_num': 0},
+	# 'ptype': 0, 'allow_all_comment': True,
+	# 'avatar_large': 'https://tvax1.sinaimg.cn/default/images/default_avatar_female_180.gif?KID=imgbed,tva&Expires=1568458610&ssig=icWnjl121I',
+	# 'avatar_hd': 'https://tvax1.sinaimg.cn/default/images/default_avatar_female_180.gif?KID=imgbed,tva&Expires=1568458610&ssig=icWnjl121I', 'verified_reason': '',
+	# 'verified_trade': '', 'verified_reason_url': '', 'verified_source': '', 'verified_source_url': '',
+	# 'follow_me': False, 'like': False, 'like_me': False, 'online_status': 0, 'bi_followers_count': 0,
+	# 'lang': 'zh-cn', 'star': 0, 'mbtype': 0, 'mbrank': 0, 'block_word': 0, 'block_app': 0, 'credit_score': 80,
+	# 'user_ability': 0, 'urank': 0, 'story_read_state': -1, 'vclub_member': 0, 'is_teenager': 0, 'is_guardian': 0, 'is_teenager_list': 0}
+
+    return HttpResponseRedirect("/manager")
 
 def signup(request):
     if request.method == 'POST':
@@ -39,6 +83,24 @@ def signup(request):
         form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form':form})
 
+def get_token(code):
+    """
+    获取access_token
+    http://open.weibo.com/wiki/Oauth2/access_token
+    """
+    app_key = "2616154578"
+    app_secret = "79a238815da135a5a0912514f7ebd93d"
+    token_url = "https://api.weibo.com/oauth2/access_token"
+    params = {
+        "client_id": app_key,
+        "client_secret": app_secret,
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": "http://127.0.0.1:8000/process"
+    }
+    result = requests.post(url=token_url, data=params)
+    print("login result --> ", result)
+    return result
 
 class MyLoginView(LoginView):
     template_name = 'accounts/login.html'
